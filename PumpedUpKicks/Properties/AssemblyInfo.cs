ea583fdde2c1023dcf7d1b634e6079628e137e73
JadeDohleman <jmodohleman@gmail.com>
2023-02-09 14:59:36 -0800

    Modified project namespaces.

--- PumpedUpKicks/Properties/AssemblyInfo.cs
+++ PumpedUpKicks/Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("JotunnModStub")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("PumpedUpKicks")]
+[assembly: AssemblyDescription("Mod for Valheim shoes")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("JotunnModStub")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyProduct("PumpedUpKicks")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- PumpedUpKicks/PumpedUpKicks.cs
+++ PumpedUpKicks/PumpedUpKicks.cs
@@ -1,23 +1,23 @@
-﻿// JotunnModStub
+﻿// PumpedUpKicks
 // a Valheim mod skeleton using Jötunn
 // 
-// File:    JotunnModStub.cs
-// Project: JotunnModStub
+// File:    PumpedUpKicks.cs
+// Project: PumpedUpKicks
 
 using BepInEx;
 using Jotunn.Entities;
 using Jotunn.Managers;
 
-namespace JotunnModStub
+namespace PumpedUpKicks
 {
     [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
     [BepInDependency(Jotunn.Main.ModGuid)]
     //[NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.Minor)]
-    internal class JotunnModStub : BaseUnityPlugin
+    internal class PumpedUpKicks : BaseUnityPlugin
     {
-        public const string PluginGUID = "com.jotunn.jotunnmodstub";
-        public const string PluginName = "JotunnModStub";
-        public const string PluginVersion = "0.0.1";
+        public const string PluginGUID = "com.jadedohleman.pumpedupkicks";
+        public const string PluginName = "PumpedUpKicks";
+        public const string PluginVersion = "0.1.0";
         
         // Use this class to add your own localization to the game
         // https://valheim-modding.github.io/Jotunn/tutorials/localization.html
@@ -26,7 +26,7 @@ namespace JotunnModStub
         private void Awake()
         {
             // Jotunn comes with its own Logger class to provide a consistent Log style for all mods using it
-            Jotunn.Logger.LogInfo("ModStub has landed");
+            Jotunn.Logger.LogInfo("PumpedUpKicks has landed");
             
             // To learn more about Jotunn's features, go to
             // https://valheim-modding.github.io/Jotunn/tutorials/overview.html
